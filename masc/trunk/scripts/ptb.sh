44222751905cd44b65f77a2a2ffe06f5bbcd7ab9
ksuderman <ksuderman@f9835c53-d872-4803-a61e-d6c59fa17448>
2010-07-21 02:52:25 +0000

    Fixed the copying to the web directory.

--- masc/trunk/scripts/ptb.sh
+++ masc/trunk/scripts/ptb.sh
@@ -4,7 +4,7 @@ source ./config.sh
 
 #cd ..
 # Convert and align the Penn Tree bank
-java $OPTS -jar $CONVERT -ptb $LOPTS -set=PTB -id=ptb -in=$IN/ptb -out=$WORK/ptb 
+#java $OPTS -jar $CONVERT -ptb $LOPTS -set=PTB -id=ptb -in=$IN/ptb -out=$WORK/ptb 
 java $OPTS -jar $ALIGN -type=ptb $LOPTS -src=$WORK/ptb -target=$OUT -dest=$OUT -fix=$FIX/ptb-fixes.xml #-skip -echo-target
 
 
