170ad7c720a565e4dceb7fa1bb0139a8e87a02e8
Keith Suderman <ksuderman@users.noreply.github.com>
2010-09-09 17:33:33 +0000

    Added check-align script to pipeline

--- scripts/link-tokens.sh
+++ scripts/link-tokens.sh
@@ -7,8 +7,8 @@ echo Linking annotations to tokens.
 LINK=./apps/link-tokens/target/LinkTokens.jar
 #LOPTS=-level=debug
 
-java $OPTS -jar $LINK $LOPTS -in=$MASC -tok=ptbtok -type=ne
-java $OPTS -jar $LINK $LOPTS -in=$MASC -tok=ptbtok -type=nc
-java $OPTS -jar $LINK $LOPTS -in=$MASC -tok=ptbtok -type=vc
+java $OPTS -jar $LINK $LOPTS -in=$MASC -tok=penn -type=ne
+java $OPTS -jar $LINK $LOPTS -in=$MASC -tok=penn -type=nc
+java $OPTS -jar $LINK $LOPTS -in=$MASC -tok=penn -type=vc
 
 
