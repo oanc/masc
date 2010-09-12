a5e95468fe7732eebb5f10d71d2131d6f70706c7
Keith Suderman <ksuderman@users.noreply.github.com>
2010-09-12 17:43:51 +0000

    Committing modified scripts.

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -14,8 +14,19 @@ java $OPTS -jar $CONVERT -xces $LOPTS -set=xces -ann=logical -in=$IN/logical -ou
 java $OPTS -jar $CONVERT -xces $LOPTS -set=xces -ann=hepple -in=$IN/penn-pos -out=$OUT -saveAs=penn -rename="Token=tok @category=msd" -id=penn
 java $OPTS -jar $CONVERT -xces $LOPTS -set=xces -ann=np -in=$IN/noun-chunks -out=$OUT -saveAs=nc -rename="np=nchunk NounChunk=nchunk" -id=nc -accept=nchunk -exf=graf:set
 java $OPTS -jar $CONVERT -xces $LOPTS -set=xces -ann=vp -in=$IN/verb-chunks -out=$OUT -saveAs=vc -rename="vp=vchunk VerbChunk=vchunk VG=vchunk" -id=vc -accept=vchunk -exf=graf:set
-java $OPTS -jar $CONVERT -xces $LOPTS -set=xces -ann=CB -in=$IN/committed-belief -out=$OUT -saveAs=cb -id=cb
 java $OPTS -jar $CONVERT -xces $LOPTS -set=xces -ann=NE -in=$IN/ne-all -out=$OUT -rename="Person=person Date=date Location=location Organization=org @orgType=type @locType=type @gender=sex" -exf="rule rule1 rule2" -id=ne -accept="person date location org" -saveAs=ne
+
+# Convert the Committed Belief data.  One file (in CB-new) also needs to be aligned.
+CBWORK=$WORK/committed-belief
+if [ -e $CBWORK ] ; then
+	rm -f $CBWORK/*.*
+else
+	mkdir -p $CBWORK
+fi
+java $OPTS -jar $CONVERT -xces $LOPTS -set=xces -ann=CB -in=$IN/committed-belief -out=$OUT -saveAs=cb -id=cb
+java $OPTS -jar $CONVERT -xces $LOPTS -set=xces -ann=CB -in=$IN/CB-new -out=$CBWORK -saveAs=cb -id=cb
+cp $IN/CB-new/*.txt $CBWORK
+java $OPTS -jar $ALIGN $LOPTS -src=$CBWORK -target=$OUT -dest=$OUT -type=cb
 
 # Convert and align the event data.
 rm -f %IN/event/graf
--- scripts/ptb.sh
+++ scripts/ptb.sh
@@ -5,8 +5,12 @@ source ./config.sh
 #cd ..
 # Convert and align the Penn Tree bank
 java $OPTS -jar $CONVERT -ptb $LOPTS -set=PTB -id=ptb -in=$IN/ptb -out=$WORK/ptb 
-#LOPTS=-log=./ptb.log\ -level=debug
-java $OPTS -jar $ALIGN -type=ptb $LOPTS -src=$WORK/ptb -target=$OUT -dest=$OUT -fix=$FIX/ptb-fixes.xml #-skip -echo-target
+java $OPTS -jar $ALIGN -type=ptb $LOPTS -src=$WORK/ptb -target=$OUT -dest=$OUT -fix=$FIX/ptb-fixes.xml 
+
+# Debugging version
+#LOPTS=-log=./ptb.log\ -level=info
+#java $OPTS -jar $ALIGN -type=ptb $LOPTS -src=$WORK/ptb -target=$OUT -dest=$OUT -fix=$FIX/ptb-fixes.xml -skip #-echo-target
+
 
 
 
