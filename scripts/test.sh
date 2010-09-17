c6b808e5ecd3498e0e20a36e83f1e5a809510eac
ide <ide@f9835c53-d872-4803-a61e-d6c59fa17448>
2010-09-17 21:48:55 +0000

    Modified scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,10 +2,11 @@
 
 source ./config.sh
 
-cd ..
-ROOT=`pwd`
-echo "ROOT is $ROOT"
+EV=$WORK/event/graf
+cp $IN/event/xces/*.txt $EV
+rm $EV/A1*.xml
+rm $EV/enron-thread*.xml
+java $OPTS -jar $ALIGN $LOPTS -src=$EV -target=$MASC -dest=$MASC -type=event -fix=$FIX/event-fixes.xml
 
-$CHECK
 echo Test complete.
 
