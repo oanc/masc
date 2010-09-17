f541e0c8cd149b687a5924161e27ad0297a09717
ide <ide@f9835c53-d872-4803-a61e-d6c59fa17448>
2010-09-17 21:48:55 +0000

    Modified scripts

--- masc/trunk/scripts/prep.sh
+++ masc/trunk/scripts/prep.sh
@@ -36,6 +36,9 @@ fi
 mkdir $EV
 cp $IN/event/xces/*.txt $EV
 java $OPTS -jar $CONVERT -xces $LOPTS -set=xces -ann=event -in=$IN/event/xces -out=$EV -saveAs=event -id=ev
+# The A1 files and enron-thread are invalid so we don't process them.
+rm $EV/A1*.xml
+rm $EV/enron-thread*.xml
 java $OPTS -jar $ALIGN $LOPTS -src=$EV -target=$MASC -dest=$MASC -type=event -fix=$FIX/event-fixes.xml
 
 # Align the opinion data
