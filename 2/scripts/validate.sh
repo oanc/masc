d5d8db7f64d841aebcd4b856a1013b34ca6fff99
frcascio <frcascio@f9835c53-d872-4803-a61e-d6c59fa17448>
2011-04-13 23:26:14 +0000

    trying to import

---
+++ 2/scripts/validate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source ./config.sh
+
+#cd ..
+SCHEMA=http://www.xces.org/ns/GrAF/1.0/graf-1.0.xsd
+VALIDATOR=$ROOT/apps/validator/target/validator.jar
+
+java $OPTS -jar $VALIDATOR -in=$MASC -schema=$SCHEMA -suffix=xml $LOPTS
+
