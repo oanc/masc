241f88fe71ec655f5d3f9701d5eb854924619c63
frcascio <frcascio@f9835c53-d872-4803-a61e-d6c59fa17448>
2011-04-13 23:26:14 +0000

    trying to import

---
+++ masc/trunk/2/scripts/link-tokens.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ./config.sh
+
+echo Linking annotations to tokens.
+
+LINK=./apps/link-tokens/target/LinkTokens.jar
+#LOPTS=-level=debug
+
+java $OPTS -jar $LINK $LOPTS -in=$MASC -tok=penn -type=ne
+java $OPTS -jar $LINK $LOPTS -in=$MASC -tok=penn -type=nc
+java $OPTS -jar $LINK $LOPTS -in=$MASC -tok=penn -type=vc
+
+
