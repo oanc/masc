262f387b379a94bfea2e1e66f05f5bf499536450
frcascio <frcascio@f9835c53-d872-4803-a61e-d6c59fa17448>
2011-04-21 03:33:34 +0000

    another copy

---
+++ 2/scipts/ptb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ./config.sh
+
+#cd ..
+# Convert and align the Penn Tree bank
+java $OPTS -jar $CONVERT -ptb $LOPTS -set=PTB -id=ptb -in=$IN/ptb -out=$WORK/ptb 
+java $OPTS -jar $ALIGN -type=ptb $LOPTS -src=$WORK/ptb -target=$OUT -dest=$OUT -fix=$FIX/ptb-fixes.xml 
+
+# Debugging version
+#LOPTS=-log=./ptb.log\ -level=info
+#java $OPTS -jar $ALIGN -type=ptb $LOPTS -src=$WORK/ptb -target=$OUT -dest=$OUT -fix=$FIX/ptb-fixes.xml -skip #-echo-target
+
+
+
+
