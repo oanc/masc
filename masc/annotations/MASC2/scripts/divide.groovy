d7e8c286c7d91f0e0849b8126998fbf79ec7f0da
ksuderman <ksuderman@f9835c53-d872-4803-a61e-d6c59fa17448>
2012-04-20 21:03:36 +0000

    Added divide groovy. Fixed graf-update.xsl

---
+++ masc/annotations/MASC2/scripts/divide.groovy
@@ -0,0 +1,78 @@
+if (this.args.length != 3)
+{
+	println "Usage: groovy divide.groovy <template directory> <source> <destination>"
+	return
+}
+
+File templateDir = new File(this.args[0])
+File source = new File(this.args[1])
+File destination = new File(this.args[2])
+
+if (!templateDir.exists())
+{	
+	println "Template directory does not exist: ${templateDir.path}"
+	return
+}
+
+if (!source.exists())
+{
+	println "Source directory not found: ${source.path}"
+	return
+}
+
+if (!destination.exists())
+{
+	println "Destination directory does not exist: ${destination}"
+	return
+}
+
+def index = [:]
+def start = templateDir.path.length() + 1
+def q = new LinkedList()
+q.addLast(templateDir)
+while (!q.isEmpty())
+{
+	def entry = q.removeFirst()	
+	if (entry.isDirectory())
+	{
+		entry.eachFile { q.addLast(it) }
+	}
+	else
+	{
+		String name = entry.name[0..-5]
+		String root = entry.parentFile.path.substring(start)
+		//println "${name} -> ${root}"
+		index[name] = root
+	}
+}
+
+source.eachFile { file ->
+	String suffix = file.name[-5..-1]
+	String name = file.name[0..-5]
+	if (file.name.endsWith('.xml'))
+	{
+		int end = file.name.lastIndexOf('-')
+		name = file.name.substring(0, end)
+	}
+	def dir = index[name]
+	if (dir)
+	{
+		println "Copying ${file.name} to ${dir}"
+		File destDir = new File(destination, dir)
+		if (!destDir.exists())
+		{
+			if (!destDir.mkdirs())
+			{
+				println "Unable to create ${destDir.path}"
+				exit 1
+			}
+		}
+		File destFile = new File(destDir, file.name)
+		String text = file.getText('UTF-8')
+		destFile.setText(text, 'UTF-8')
+	}
+	else
+	{
+		println "No entry found for ${file.name}"
+	}
+}
