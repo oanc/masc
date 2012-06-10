8d41f6cc4b80ae013ace38a0f0e0a9e6dbf8b472
Keith Suderman <ksuderman@users.noreply.github.com>
2012-06-10 17:44:23 +0000

    Tweaks for MASC 2/3 processing.

--- scripts/corrections2.sh
+++ scripts/corrections2.sh
@@ -17,23 +17,26 @@ fi
 
 mkdir $TEMP
 
+# Converts the GrAF files to XCES and puts them in the TEMP directory.
 grate scripts/corrections.gr8 $IN/txtfiles $GRAF $TEMP
+# Converts files in the TEMP directory and places them in the data/data directory.
 java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -exf="graf:id graf:set" -id=nc -ann=nc -rename="np=nchunk NounChunk=nchunk" -accept=nchunk 
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -exf="graf:id graf:set" -id=vc -ann=vc -rename="VG=vchunk"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -exf="graf:id graf:set" -id=vc -ann=vc -rename="VG=vchunk vp=vchunk VerbChunk=vchunk"
 java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -exf="graf:id graf:set" -id=ne -ann=ne -rename="Person=person Date=date Location=location Organization=org @orgType=type @locType=type @gender=sex" -exf="rule rule1 rule2" -accept="person date location org"
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -exf="graf:id graf:set" -id=penn -ann=penn -rename="Token=tok @category=msd" -saveAs=penn
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -ann=s -id=s -rename="Sentence=s"
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -ann=logical -id=logical -rename="@speaker=who"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -exf="graf:id graf:set" -id=penn -ann=penn -rename="Token=tok @category=msd" -saveAs=penn -exf="string graf:set graf:id graf:edge"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -ann=s -id=s -rename="Sentence=s" -exf="graf:set graf:id graf:edge"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$TEMP -out=$OUT -set=xces -ann=logical -id=logical -rename="@speaker=who" -exf="graf:set graf:id graf:edge"
 rm -rf $TEMP
 #exit
 
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -set=xces -ann=s -id=s -rename="Sentence=s"
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -set=xces -ann=logical -id=logical -rename="@speaker=who"
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -set=xces -ann=hepple -id=penn -rename="Token=tok @category=msd" -saveAs=penn
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -set=xces -ann=penn -id=penn -rename="Token=tok @category=msd" -saveAs=penn
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -ann=nc -set=xces -rename="np=nchunk NounChunk=nchunk" -id=nc -accept=nchunk -exf=graf:set
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -ann=vc -set=xces -rename="VG=vchunk" -id=vc -accept=vchunk -exf=graf:set
-java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -ann=ne -set=xces -rename="Person=person Date=date Location=location Organization=org @orgType=type @locType=type @gender=sex" -exf="rule rule1 rule2" -id=ne -accept="person date location org"
+# Convert the XCES files.
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -set=xces -ann=s -id=s -rename="Sentence=s" -exf="graf:set graf:id graf:edge"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -set=xces -ann=logical -id=logical -rename="@speaker=who" -exf="graf:set graf:id graf:edge"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -set=xces -ann=hepple -id=penn -rename="Token=tok @category=msd" -saveAs=penn -exf="string graf:set graf:id graf:edge"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -set=xces -ann=penn -id=penn -rename="Token=tok @category=msd" -saveAs=penn -exf="string graf:set graf:id graf:edge"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -ann=nc -set=xces -rename="np=nchunk NounChunk=nchunk" -id=nc -accept=nchunk -exf="graf:set graf:id graf:edge"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -ann=vc -set=xces -rename="VG=vchunk vp=vchunk VerbChunk=vchunk" -id=vc -accept=vchunk -exf="graf:set graf:id graf:edge"
+java $OPTS -jar $CONVERT -xces $LOPTS -in=$XCES -out=$OUT -ann=ne -set=xces -rename="Person=person Date=date Location=location Organization=org @orgType=type @locType=type @gender=sex" -exf="rule rule1 rule2 graf:set graf:id graf:edge" -id=ne -accept="person date location org"
 
 
 
--- scripts/tokenize.sh
+++ scripts/tokenize.sh
@@ -5,8 +5,8 @@ source ./config.sh
 
 #LOPTS=-log=./release.log\ -level=trace\ -append
 # Perform quarkification
-java $OPTS -cp $SPLITTER org.anc.graf.splitter.App -in=$OUT -out=$WORK/tokens/ptb -type=ptb -set=PTB $LOPTS
-java $OPTS -cp $SPLITTER org.anc.graf.splitter.App -in=$OUT -out=$WORK/tokens/fn -type=fn -set=FrameNet $LOPTS
+java $OPTS -cp $SPLITTER org.anc.graf.splitter.App -in=$OUT -out=$WORK/tokens/ptb -type=ptb -set=ptb $LOPTS
+java $OPTS -cp $SPLITTER org.anc.graf.splitter.App -in=$OUT -out=$WORK/tokens/fn -type=fn -set=fn $LOPTS
 
 java $OPTS -cp $SPLITTER org.anc.graf.tokens.MergeTokens -penn=$OUT -ptb=$WORK/tokens/ptb -fn=$WORK/tokens/fn -out=$WORK/tokens/quarks $LOPTS # -source=ptb -target=fn
 
