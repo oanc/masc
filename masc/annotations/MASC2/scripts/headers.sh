f7649900cb0723370caef8e52be8ccad1bc0aca6
ksuderman <ksuderman@f9835c53-d872-4803-a61e-d6c59fa17448>
2012-06-13 14:11:50 +0000

    Fixes to header generation.

--- masc/annotations/MASC2/scripts/headers.sh
+++ masc/annotations/MASC2/scripts/headers.sh
@@ -4,7 +4,7 @@ source ./config.sh
 
 echo Generating headers.
 JAR=$APPS/masc-headers/target/masc-headers.jar
-java $OPTS -jar $JAR $CORPORA $IN/MASC-MASTER.csv $DATA/headers
+java $OPTS -jar $JAR $CORPORA 2 $IN/MASC-MASTER.csv $DATA/headers
 
 # The above generates MASC 3 headers as well. Copy the MASC 2
 # headers into the output directory
