9aeaa102fbb865af398924a89b3a7d25e494ffdd
Keith Suderman <ksuderman@users.noreply.github.com>
2012-09-17 14:16:47 +0000

    Modified fix-ids.sh to be able to use MASC 3 check-ids program.

--- scripts/fix-ids.sh
+++ scripts/fix-ids.sh
@@ -2,6 +2,7 @@
 
 source ./config.sh
 #cd ..
+
 APP=./apps/check-ids/target/CheckIds.jar
 DEST=./data/working/ids
 
@@ -11,6 +12,16 @@ else
 	rm -f $DEST/*.*
 fi
 
-java $OPTS -jar $APP $LOPTS -in=$OUT -out=$DEST -filter=-s.xml -id=s
+# The name of the jar varies depending on the Subversion branch.
+if [ -f $APP ] ; then
+	java $OPTS -jar $APP $LOPTS -in=$OUT -out=$DEST -filter=-s.xml -id=s
+else
+	APP=./apps/check-ids/target/check-ids.jar
+	if [ ! -f $APP ] ; then
+		echo "Unable to find the jar file for the check-ids application."
+		exit 1
+	fi
+	java $OPTS -jar $APP $LOPTS -in=$OUT -out=$DEST #-filter=-s.xml -id=s
+fi
 cp -f $DEST/*.* $OUT
 
