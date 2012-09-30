a2c5bcbff701b4349822eef8b9140ef1310dd6dd
ksuderman <ksuderman@f9835c53-d872-4803-a61e-d6c59fa17448>
2012-09-30 19:18:06 +0000

    Added fix-strings Groovy script.

--- masc/annotations/MASC3/scripts/check-tokens.sh
+++ masc/annotations/MASC3/scripts/check-tokens.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# NOTE: The check-tokens program is typically run from the reports.sh script.
+
 source ./config.sh
 
 set -eu
 
-CHECK_TOKENS=$APPS/check-tokens/target/check-tokens.jar
+TOKENS=$APPS/check-tokens/target/check-tokens.jar
 REPORT=./reports/check-tokens
 if [ ! -e $REPORT ] ; then
 	mkdir -p $REPORT
 fi
 
-java $OPTS -jar $CHECK_TOKENS $LOPTS -in=$RELEASE/data -header=$RELEASE/resource-header.xml -out=$REPORT
+#java $OPTS -jar $CHECK_TOKENS $LOPTS -in=$RELEASE/data -header=$RELEASE/resource-header.xml -out=$REPORT
+java -Xmx2G -jar $TOKENS $LOPTS -in=$RELEASE/data -out=./reports -header=$METADATA/MASC3-resource-header.xml
---
+++ masc/annotations/MASC3/scripts/fix-strings.groovy
@@ -0,0 +1,76 @@
+@Grab(group='org.tc37sc4.graf', module='graf-api', version='1.2.0')
+import org.xces.graf.api.*
+@Grab(group='org.tc37sc4.graf', module='graf-io', version='1.2.0')
+import org.xces.graf.io.*
+import org.xces.graf.io.dom.*
+@Grab(group='org.tc37sc4.graf', module='graf-impl', version='1.2.0')
+import org.xces.graf.impl.*
+@Grab(group='org.tc37sc4.graf', module='graf-util', version='1.2.0')
+import org.xces.graf.util.*
+
+def files = ['./written/govt-docs/chapter-10', './written/non-fiction/ch5', './written/journal/VOL15_3']
+
+if (this.args.size() != 2)
+{
+	println "USAGE"
+	println ""
+	println "groovy fix-strings.groovy /path/to/resource-header.xml /path/to/data"
+	println ""
+	return
+}
+
+def dataDir = new File(this.args[1])
+if (!dataDir.exists())
+{
+	println "Data directory not found."
+	return
+}
+
+if (!dataDir.isDirectory())
+{
+	println "Specified location is not a directory."
+	return
+}
+
+def headerFile = new File(this.args[0])
+if (!headerFile.exists())
+{
+	println "Unable to locate the resource header."
+	return
+}
+
+ResourceHeader header = new ResourceHeader(headerFile)
+GrafParser parser = new GrafParser(header)
+
+files.each { filename ->
+	def pennFile = new File(dataDir, filename + '-penn.xml')
+	if (!pennFile.exists())
+	{
+		println "Unable to find ${pennFile.path}"
+	}
+	else
+	{
+		println "Updating @strings for ${pennFile.path}"
+		def txtFile = new File(dataDir, filename + '.txt')
+		String text = txtFile.getText('UTF-8')
+		IGraph graph = parser.parse(pennFile)
+		graph.nodes.each { node ->
+			IRegion region = GraphUtils.getSpan(node)
+			int start = GraphUtils.getOffset(region.start)
+			int end = GraphUtils.getOffset(region.end)
+			node.annotations().each { a ->
+				if (a.label == 'tok')
+				{
+					def substring = text.substring(start, end)
+					//println "${node.id} ${a.label} ${start}-${end} ${substring}"
+					a.addFeature('string', substring)
+				}
+			}
+		}
+		FileOutputStream stream = new FileOutputStream(pennFile)
+		IRenderer renderer = new GrafRenderer(stream, 'UTF-8')
+		renderer.render(graph)
+		stream.flush()
+		stream.close()
+	}
+}
